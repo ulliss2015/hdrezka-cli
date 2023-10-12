0ad2c4432693e0262d9be05626e9c3d555fde21c
ulliss2015 <37631333+ulliss2015@users.noreply.github.com>
2023-10-12 20:22:49 +0300

    Add files via upload

---
+++ hdrezka.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Активируйте виртуальное окружение
+source .venv/bin/activate
+
+# Запустите ваш Python-скрипт
+python "hdrezka-cli.py"
+
+# Деактивируйте виртуальное окружение (по желанию)
+deactivate
+
